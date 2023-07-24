07f8e3778b9602e3cbb06bd0d45fff352edacbed
Muhammed ERASLAN <muhammed.eraslan@monsternotebook.com>
2023-07-24 17:28:56 +0300

    fixed

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import ResponsiveCarousel from "../components/Responsive/ResponsiveCarousel";
 import Banner from "../components/Banner";
 import UserComment from "../components/UserComment";
 
-export default function Home() {
+export default function Home({children:never}) {
   return (
     <div className=" max-lg:overflow-hidden ">
       <Carousel autoSlide={true}></Carousel>
@@ -13,3 +13,4 @@ export default function Home() {
     </div>
   );
 }
+
