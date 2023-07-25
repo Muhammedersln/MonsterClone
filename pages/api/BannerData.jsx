12aa61a4b5299b396262ac0b10cbf3a12cd7f020
Muhammed ERASLAN <muhammed.eraslan@monsternotebook.com>
2023-07-25 11:47:41 +0300

    api fixed

--- pages/api/BannerData.jsx
+++ pages/api/BannerData.jsx
@@ -1,32 +1,16 @@
 import axios from "axios";
-const BanerData = async () => {
 
+const BanerData = async () => {
   try {
     const body = {
-      bannerCode: 7337,
+      bannerCode: 7612,
       productId: 0,
       categoryId: 0,
     };
 
-    const response = await axios.post(
-      "https://api.monsternotebook.com/ecommerce-api/v1/banner/banner-filter",
-      body,
-      {
-        headers: {
-          "x-monster-client-channel": "MobileAppTr",
-          "Content-Type": "application/json",
-          "Cache-Control": "no-cache",
-          "Ocp-Apim-Subscription-Key": "23e9ecb898a74612935b48353804506c",
-          "Access-Control-Allow-Credentials":true,
-          "Access-Control-Allow-Origin":"*",
-          "Access-Control-Allow-Methods":"GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          "Access-Control-Allow-Headers":"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    const response = await axios.post("/api/banner-filter", body);
 
-          
-        },
-      }
-    );
-      return response.data
+    return response.data;
   } catch (error) {
     console.error(error);
   }
