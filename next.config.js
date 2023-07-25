c313505b0c86abf457094faa9aaf0724b089a57a
Muhammed ERASLAN <muhammed.eraslan@monsternotebook.com>
2023-07-25 13:35:19 +0300

    fixedddd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Match all pages
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Replace with the allowed origin, or "*" to allow all origins (not recommended in production)
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS", // Add the allowed HTTP methods
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization", // Add the allowed headers
+          },
+        ],
+      },
+    ];
+  },
     env: {
       WEB_URL: process.env.WEB_URL,
       API_URL:process.env.API_URL,
